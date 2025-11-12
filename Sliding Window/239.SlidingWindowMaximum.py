a3d06aee7f24d5ce807c223a542259c323b03ed8
Banana-4 <stig34768@gmail.com>
2025-11-12 17:35:31 +0100

    update 239. SlidingWindowMaximum
    Change to a deque algorithm.

--- Sliding Window/239.SlidingWindowMaximum.py
+++ Sliding Window/239.SlidingWindowMaximum.py
@@ -1,22 +1,14 @@
-import heapq
-
+from collections import deque
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        left, right = 0, 0
-        max_n =(left, nums[left])
-        while right < k:
-            if max_n[1] < nums[right]:
-                max_n =(right, nums[right])
-            right += 1
-        heap = []
-        for i in range(max_n[0], right):
-            heapq.heappush(heap, (-nums[i], i))
-        result = [-heap[0][0]]
-        while right < len(nums):
-            heapq.heappush(heap, (-nums[right], right))
-            left += 1
-            right +=1
-            while heap[0][1] < left:
-                heapq.heappop(heap) 
-            result.append(-heap[0][0])
+        dq = deque()
+        result = []
+        for i, n in enumerate(nums):
+            while dq and nums[dq[-1]] < nums[i]:
+                dq.pop()
+            dq.append(i)
+            if dq[0] < i - k + 1:
+                dq.popleft()
+            if i >= k - 1:
+                result.append(nums[dq[0]])
         return result
