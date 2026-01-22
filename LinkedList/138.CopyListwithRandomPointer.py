e07f3c4d5f7b68e82600f8d58e31d74528c77f67
Banana-4 <stig34768@gmail.com>
2026-01-22 19:51:26 +0100

    remove the dummy and simplify the logic

--- LinkedList/138.CopyListwithRandomPointer.py
+++ LinkedList/138.CopyListwithRandomPointer.py
@@ -1,17 +1,14 @@
 class Solution:
-    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        newHead = Node(-1, None, None)
-        node = newHead
-        prev = newHead
-        while head:
-            node = Node(head.val, head.next, head.random)
-            prev.next = node
-            prev = node
-            head.copy = node
-            head = head.next
-        newHead = newHead.next
-        node = newHead
-        while node:
-            node.random = node.random.copy if node.random else None
-            node = node.next
-        return newHead
+    def copyRandomList(self, head: "Optional[Node]") -> "Optional[Node]":
+        current = head
+        groups = {}
+        while current:
+            node = Node(current.val)
+            groups[current] = node
+            current = current.next
+        current = head
+        while current:
+            groups[current].random = groups.get(current.random, None)
+            groups[current].next = groups.get(current.next, None)
+            current = current.next
+        return groups.get(head, None)
