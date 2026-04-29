c0179f4f55b5d5d73b51951c9765265311440be4
Banana-4 <stig34768@gmail.com>
2026-04-29 10:32:38 +0000

    Add LCA algorithm for binary tree
    Messy and to complex logic

---
+++ Trees/236.LCA_of_BT.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def DFS(self, root, p, q, found, path):
+        if not root:
+            return
+        if root.val == p:
+            found[0] = True
+            if not found[1]:
+                path.append(root)
+        if root.val == q:
+            found[1] = True
+            if not found[0]:
+                path.append(root)
+        if found[0] and found[1]:
+            return
+        if not found[1] and not found[0]:
+            path.append(root) 
+        self.DFS( root.left, p, q, found, path)
+        if found[0] and found[1]:
+            return
+        self.DFS( root.right, p, q, found, path)
+        if found[0] and found[1]:
+            return
+        if root == path[-1]:
+            path.pop()
+
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        path = []
+        found = [False, False]
+        self.DFS( root, p.val, q.val, found, path)
+        return path[-1]
